05edd62c8c403567a17f86ce67f3e6d8d779dd2d
santiagomenesses <72476704+santiagomenesses@users.noreply.github.com>
2021-10-10 13:40:30 -0500

    Added auth and register
    :3

--- app/src/main/java/main/Pantallas/PrincipalActivity.java
+++ app/src/main/java/main/Pantallas/PrincipalActivity.java
@@ -1,13 +1,18 @@
 package main.Pantallas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -15,13 +20,23 @@ public class PrincipalActivity extends AppCompatActivity {
     Button botonHistorias;
     Button chat;
     ImageView imagenEjemplo;
-
+    ImageButton signOut;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
+        mAuth= FirebaseAuth.getInstance();
         inflate();
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent i= new Intent(getApplicationContext(),MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
 
         imagenEjemplo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +45,7 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });
     }
+
 
     public void launchPostsActivity(View v){
         startActivity(new Intent(this, PostsActivity.class));
@@ -44,6 +60,11 @@ public class PrincipalActivity extends AppCompatActivity {
         botonCrearPost = findViewById(R.id.botonPost);
         botonHistorias = findViewById(R.id.botonHistorias);
         chat = findViewById(R.id.botonChat);
+<<<<<<< Updated upstream
         imagenEjemplo = findViewById(R.id.imagenEjemplo);
+=======
+        imagenEjemplo = findViewById(R.id.imageView6);
+        signOut=findViewById((R.id.signOut));
+>>>>>>> Stashed changes
     }
 }
