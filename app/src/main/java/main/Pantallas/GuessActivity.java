bc98a3449a0b5fafa353bb985f132b775dc8c45d
danim <dani.monsalveg@gmail.com>
2021-10-12 18:07:35 -0500

    Actualizado el mapa

--- app/src/main/java/main/Pantallas/GuessActivity.java
+++ app/src/main/java/main/Pantallas/GuessActivity.java
@@ -1,5 +1,6 @@
 package main.Pantallas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -9,17 +10,33 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.service.quickaccesswallet.GetWalletCardsCallback;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -29,90 +46,183 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.TilesOverlay;
+
+import java.io.IOException;
+import java.util.List;
 
 public class GuessActivity extends AppCompatActivity {
 
-    MapView map;
     TextView tvNombre;
-    private FusedLocationProviderClient fusedLocationClient;
-    public static final int PERMISSION_LOCATION = 0;
-    GeoPoint ubicacion = new GeoPoint(4.62,-74.07);
-    Marker marker;
+    private static final double RADIUS_OF_EARTH_KM = 6371;
+    MapView map;
+    private FusedLocationProviderClient locationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    static final int LOCATION_REQ = 0;
+    GeoPoint location;
+    Marker locationMarker;
+    Marker longPressedMarker;
+    Task<LocationSettingsResponse> task;
+    Geocoder geocoder;
+    SensorManager manager;
+    Sensor lightSensor;
+    SensorEventListener lightSensorListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
-        //inflate
-        map = findViewById(R.id.mapView);
-        tvNombre = findViewById(R.id.textViewNombre);
-        //Obtener ubicacion
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, "", PERMISSION_LOCATION);
-        obtenerUbicacion();
-        //iniciar mapa
-        Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx,PreferenceManager.getDefaultSharedPreferences(ctx));
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setMultiTouchControls(true);
-        //Obtener Ubicacion
-        obtenerUbicacion();
-        marker = new Marker(map);
-        marker.setTitle("Marcador");
-        Drawable icon = getResources().getDrawable(R.drawable.location_blue,this.getTheme());
-        marker.setIcon(icon);
-        marker.setPosition(ubicacion);
-        marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
-        IMapController controller = map.getController();
-        controller.setZoom(18.0);
-        controller.setCenter(ubicacion);
-        map.getOverlays().add(marker);
+
+        requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, "location", LOCATION_REQ);
+
+        inflate();
+
+        checkGPS();
+
+        initializeMarker();
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED)
+                    startLocationUpdates();
+            }
+        });
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Location l = locationResult.getLastLocation();
+                location.setLongitude(l.getLongitude());
+                location.setLatitude(l.getLatitude());
+                location.setAltitude(l.getAltitude());
+                if(locationMarker != null)
+                    map.getOverlays().remove(locationMarker);
+                locationMarker = createMarker(location,"Estas aqui",null);
+                map.getOverlays().add(locationMarker);
+                map.invalidate();
+            }
+        };
+        lightSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                Log.d("SENSOR","Entre: "+sensorEvent.values[0]);
+                if(sensorEvent.values[0] < 5000) {
+                    Log.d("SENSOR", "Estoy con menos de 5000");
+                    map.getOverlayManager().getTilesOverlay().setColorFilter(TilesOverlay.INVERT_COLORS);
+                }
+                else{
+                    Log.d("SENSOR","Estoy con mas o igual de 5000");
+                    //map.getOverlayManager().getTilesOverlay().setColorFilter(TilesOverlay.INVERT_COLORS);
+                }
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+        manager.registerListener(lightSensorListener,lightSensor,SensorManager.SENSOR_DELAY_FASTEST);
+        map.getOverlays().add(createOverlayEvents());
     }
 
-    private void obtenerUbicacion() {
-        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+    private MapEventsOverlay createOverlayEvents() {
+        MapEventsOverlay eventsOverlay = new MapEventsOverlay(new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                longPressOnMap(p);
+                return true;
+            }
+        });
+        return eventsOverlay;
+    }
+
+    private void longPressOnMap(GeoPoint p) {
+        if(longPressedMarker!=null)
+            map.getOverlays().remove(longPressedMarker);
+        String locationName;
+        try {
+            List<Address> list = geocoder.getFromLocation(p.getLatitude(),p.getLongitude(),2);
+            if(!list.isEmpty()) {
+                Address result = list.get(0);
+                locationName = result.getAddressLine(0);
+                longPressedMarker = createMarker(p, locationName, null);
+                map.getOverlays().add(longPressedMarker);
+                String dist = String.valueOf(distance(location.getLatitude(),location.getLongitude()
+                        ,p.getLatitude(),p.getLongitude()));
+                Toast.makeText(this,"Distancia hacia allá: "+dist,Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeMarker() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        ubicacion.setLatitude(location.getLatitude());
-                        ubicacion.setLongitude(location.getLongitude());
+                public void onSuccess(Location loc) {
+                    if (loc != null) {
+                        location = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+                        locationMarker = createMarker(location,"Estas aquí","");
+                        map.getOverlays().add(locationMarker);
+                        map.getController().setCenter(location);
                     }
                 }
             });
         }
     }
 
+    private Marker createMarker(GeoPoint p, String title, String desc){
+        Marker marker = new Marker(map);
+        marker.setPosition(p);
+        marker.setIcon(ContextCompat.getDrawable(GuessActivity.this, R.drawable.location_black));
+        marker.setImage(ContextCompat.getDrawable(GuessActivity.this, R.drawable.location_black));
+        marker.setTitle(title);
+        marker.setSubDescription(desc);
+        return marker;
+    }
+
+    private void checkGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        task = client.checkLocationSettings(builder.build());
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         map.onResume();
-        obtenerUbicacion();
+        startLocationUpdates();
         IMapController controller = map.getController();
         controller.setZoom(18.0);
-        controller.setCenter(ubicacion);
+        controller.setCenter(this.location);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         map.onPause();
+        stopLocationUpdates();
+        manager.unregisterListener(lightSensorListener,lightSensor);
     }
 
-    private Marker createMarker(GeoPoint p, String title, String desc, int iconID){
-        Marker marker = null;
-        if(map!=null) {
-            marker = new Marker(map);
-            if (title != null) marker.setTitle(title);
-            if (desc != null) marker.setSubDescription(desc);
-            if (iconID != 0) {
-                Drawable myIcon = getResources().getDrawable(iconID, this.getTheme());
-                marker.setIcon(myIcon);
-            }
-            marker.setPosition(p);
-            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+    private void stopLocationUpdates() {
+        locationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper());
         }
-        return marker;
     }
 
     private void requestPermission(Activity context, String permission, String justification, int id){
@@ -126,5 +236,39 @@ public class GuessActivity extends AppCompatActivity {
             // request the permission.
             ActivityCompat.requestPermissions(context, new String[]{permission}, id);
         }
+    }
+
+    private void inflate() {
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        map = findViewById(R.id.mapView);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = createLocationRequest();
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        geocoder = new Geocoder(getBaseContext());
+    }
+
+    private LocationRequest createLocationRequest() {
+        return LocationRequest.create()
+                .setInterval(10000)
+                .setFastestInterval(5000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    public void centerCamera(View v){
+        map.getController().setCenter(location);
+    }
+
+    public double distance(double lat1, double long1, double lat2, double long2) {
+        double latDistance = Math.toRadians(lat1 - lat2);
+        double lngDistance = Math.toRadians(long1 - long2);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double result = RADIUS_OF_EARTH_KM * c;
+        return Math.round(result*100.0)/100.0;
     }
 }
