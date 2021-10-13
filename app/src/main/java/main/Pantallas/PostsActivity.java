c2021201bf9c7628b1d421239a7cf1680726cdf4
ayus <rojasnpaula@javeriana.edu.co>
2021-10-12 22:49:47 -0500

    temperature

--- app/src/main/java/main/Pantallas/PostsActivity.java
+++ app/src/main/java/main/Pantallas/PostsActivity.java
@@ -2,14 +2,51 @@ package main.Pantallas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class PostsActivity extends AppCompatActivity {
+public class PostsActivity extends AppCompatActivity  implements SensorEventListener {
+
+    private SensorManager mgr;
+    private Sensor temp;
+    private TextView text;
+    private StringBuilder msg = new StringBuilder(2048);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+        mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        temp = mgr.getDefaultSensor( Sensor.TYPE_AMBIENT_TEMPERATURE);
+        text = (TextView) findViewById(R.id.text);
+
+    }
+    @Override
+    protected void onResume() {
+        mgr.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL);
+
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        mgr.unregisterListener(this, temp);
+        super.onPause();
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        text.setText("");
+        float fahrenheit = event.values[0] * 9 / 5 + 32;
+        msg.insert(0, event.values[0] + " Celsius");
+        text.setText(msg);
+        System.out.println(msg);
+        text.invalidate();
     }
 }
